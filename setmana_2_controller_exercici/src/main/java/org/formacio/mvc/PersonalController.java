2099dc4402ef9faa07ca62560aa8ec25a6340632
Unknown <juanan.pujals@gmail.com>
2018-05-03 18:21:33 +0200

    primer caso test pasa

--- setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
+++ setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
 
+@Component	
 public class PersonalController {
 
 	// no toqueu la declaracio de baseDeDades ni el metode getBaseDeDades
