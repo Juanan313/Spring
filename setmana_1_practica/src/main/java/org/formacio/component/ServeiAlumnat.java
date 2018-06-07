80c565f8b74f28c928f28843afc05486ea048c49
Juan Antonio Pujals Echeverría <jpujals@quonext-tur.com>
2018-06-07 12:22:37 +0200

    testsMatricula Passats

--- setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
+++ setmana_1_practica/src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,11 +1,21 @@
 package org.formacio.component;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
 
-	
+	@Autowired
+	RepositoriAlumnes repositoriAlumnes;
+
+	@PostConstruct
+	private void creacioDadesInicials(){
+		repositoriAlumnes.altaAlumne(1, "Antonia");
+		repositoriAlumnes.altaAlumne(2, "Joan");
+	}
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
 	 * el corresponent codi.
@@ -13,7 +23,12 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
-		return true;
+
+		if (alumne != null) {
+			repositoriAlumnes.altaAlumne(id, alumne);
+			return true;
+		}
+		return false;
 	}
 	
 }
